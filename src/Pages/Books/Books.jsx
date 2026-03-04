48ae5b142d344e95aac982154b835468820e8d54
bmfaysal33 <bmfaysal33@gmail.com>
2026-03-04 11:53:27 +0600

    rendered data from react router and showing

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,19 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import Book from "../Book/Book";
 
-const Books = () => {
-    const [allBooks , setAllBooks]= useState([]);
-    
-    useEffect(()=> {
-        fetch("booksData.json")
-        .then(res => res.json())
-        .then(data => setAllBooks(data))
+// Data Load Method 1
 
-    },[])
+const Books = ({ data }) => {
+  // Data Load Method 1 --- UseEffect
 
+  //     const [allBooks , setAllBooks]= useState([]);
+
+  //     useEffect(()=> {
+  //         fetch("booksData.json")
+  //         .then(res => res.json())
+  //         .then(data => setAllBooks(data))
+
+  //     },[])
+
+  // Data Load Method 2
+
+  // const bookPromise = fetch('booksData.json').then((res) => res.json());
+
+  //  steps:
+  // i> create suspense ii> enter the component which to render
+  // iii> pass the promise iv> then load by (use) ; done
+
+  // Data load method 3
+
+  // i> pass from Route
 
   return (
-    <div>Hello I'm Books.... </div>
-  )
-}
+    <div>
+      <h1 className="text-4xl text-center p-6"> Books </h1>
+      <Suspense fallback={<span> Loading....</span>}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {data.map((singleBook) => (
+            <Book key={singleBook.bookId} singleBook={singleBook}></Book>
+          ))}
+        </div>
+      </Suspense>
+    </div>
+  );
+};
 
-export default Books
+export default Books;
